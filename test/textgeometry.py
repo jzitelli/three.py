db93c8f0a74fe8d4bb32d48b0dd9b3bceaa80b22
Jeffrey Zitelli <jeffrey.zitelli@gmail.com>
2016-02-11 12:35:31 -0500

    tests

---
+++ test/textgeometry.py
@@ -0,0 +1,55 @@
+import logging
+_logger = logging.getLogger(__name__)
+import json
+from flask import Flask, Blueprint, render_template, Markup
+from needle.cases import NeedleTestCase
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.split(__file__)[0], os.path.pardir)))
+import site_settings
+import pyserver
+from three import *
+from flask_app import main
+
+
+blueprint = Blueprint("test_text", __name__,
+                      static_folder=site_settings.STATIC_FOLDER,
+                      static_url_path='',
+                      template_folder=site_settings.TEMPLATE_FOLDER)
+
+
+
+@blueprint.route('/test_text')
+def _test_text():
+    scene = Scene()
+    scene.add(Mesh(geometry=TextGeometry(text='test_text',
+                                         font_url="node_modules/three.js/fonts/helvetiker_regular.typeface.js",
+                                         size=0.1, height=0),
+                   material=MeshBasicMaterial(color=0xff00ff),
+                   position=[0, 0, -2]))
+    return render_template('index.html',
+                           json_config=Markup(r"""<script>
+var THREEPY_SCENE = %s;
+</script>""" % json.dumps(scene.export(), indent=2)))
+
+
+
+class TextGeometryTest(NeedleTestCase):
+    def test_screenshot(self):
+        self.driver.get('127.0.0.1:5000/test_text')
+        self.assertScreenshot('canvas', 'test_text')
+
+
+
+if __name__ == "__main__":
+    import logging
+    app = Flask(__name__,
+                static_folder=site_settings.STATIC_FOLDER,
+                static_url_path='',
+                template_folder=site_settings.TEMPLATE_FOLDER)
+    app.debug = site_settings.DEBUG
+    app.testing = True
+    app.register_blueprint(blueprint)
+    logging.basicConfig(level=(logging.DEBUG if app.debug else logging.INFO),
+                        format="%(levelname)s %(name)s %(funcName)s %(lineno)d:  %(message)s")
+    app.run(host='0.0.0.0')
