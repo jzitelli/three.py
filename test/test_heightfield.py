16f00d880892bd23f12b5a1f904d58c7131819bd
Jeffrey Zitelli <zemedi@gmail.com>
2016-02-11 21:17:34 -0500

    test_heightfield

--- test/test_heightfield.py
+++ test/test_heightfield.py
@@ -1,22 +1,31 @@
 import json
-import numpy as np
 from needle.cases import NeedleTestCase
-
+from flask import Blueprint, Flask, Markup, render_template
 import os.path
 import sys
 THREEPYDIR = os.path.abspath(os.path.join(os.path.split(__file__)[0], os.path.pardir))
 if THREEPYDIR not in sys.path:
     sys.path.insert(0, THREEPYDIR)
-from pyserver.flask_app import app, request, Markup, render_template, main
+import pyserver
+import site_settings
+from flask_app import WebVRConfig
 from three import *
 
 
-@app.route('/test/heightfield')
+
+blueprint = Blueprint('/test_layers', __name__,
+                      static_folder=site_settings.STATIC_FOLDER,
+                      static_url_path='',
+                      template_folder=site_settings.TEMPLATE_FOLDER)
+
+
+
+@blueprint.route('/test_heightfield')
 def _test_heightfield():
     scene = Scene()
     scene.add(PointLight(color=0xffffff, intensity=1, distance=800,
                          position=[10, 0, 0]))
-    heightfieldImage = Image(url='../images/terrain128.png')
+    heightfieldImage = Image(url='images/terrain128.png')
     scene.add(HeightfieldMesh(heightfieldImage=heightfieldImage,
                               heightfieldScale=32,
                               material=MeshLambertMaterial(color=0xff0000),
@@ -33,21 +42,30 @@ def _test_heightfield():
                    cannonData={'mass': 1, 'shapes': ['Box']}))
     return render_template('index.html',
                            json_config=Markup(r"""<script>
-var THREE_PY_CONFIG = %s;
-var JSON_SCENE = %s;
-</script>""" % (json.dumps({'controls': request.args.get('controls')}, indent=2),
+var WebVRConfig = %s;
+var THREEPY_SCENE = %s;
+</script>""" % (json.dumps(WebVRConfig, indent=2),
                 json.dumps(scene.export(), indent=2))))
+
 
 
 class HeightfieldTest(NeedleTestCase):
     def test_screenshot(self):
-        self.driver.get('127.0.0.1:5000/test/heightfield')
-        self.assertScreenshot('canvas', 'heightfield_screenshot')
+        self.driver.get('127.0.0.1:5000/test_heightfield')
+        self.assertScreenshot('canvas', 'test_heightfield')
+
 
 
 if __name__ == "__main__":
     import logging
+    app = Flask(__name__,
+                static_folder=site_settings.STATIC_FOLDER,
+                static_url_path='',
+                template_folder=site_settings.TEMPLATE_FOLDER)
+    app.debug = site_settings.DEBUG
+    app.testing = True
+    app.register_blueprint(pyserver.blueprint)
+    app.register_blueprint(blueprint)
     logging.basicConfig(level=(logging.DEBUG if app.debug else logging.INFO),
                         format="%(levelname)s %(name)s %(funcName)s %(lineno)d:  %(message)s")
-    app.config['TESTING'] = True
-    main()
+    app.run('0.0.0.0')
