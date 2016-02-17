338c4a3a2764ebcc6928033913f3b3c98f6f14c8
Jeffrey Zitelli <jeffrey.zitelli@gmail.com>
2016-02-17 11:10:59 -0500

    dumb fix for poolvr

--- three/buffer_geometries.py
+++ three/buffer_geometries.py
@@ -41,7 +41,7 @@ class BufferGeometry(Three):
 
 
 def _tri_faces(rect_face, flip_normals=False):
-    "Return indices for two triangles comprising the rectangle"
+    "Return indices for two triangles comprising the quadrilateral"
     if flip_normals:
         return [[rect_face[0], rect_face[2], rect_face[1]], [rect_face[0], rect_face[3], rect_face[2]]]
     else:
@@ -56,13 +56,14 @@ class QuadBufferGeometry(BufferGeometry):
 
 
 class HexaBufferGeometry(BufferGeometry):
+    # TODO: check if quads are coplanar
     def __init__(self, vertices, **kwargs):
         rects = [[0,1,2,3][::-1], # bottom
                  [4,5,6,7], # top
                  [0,1,5,4], # front
                  [1,2,6,5], # right
                  [2,3,7,6], # rear
-                 [3,0,4,7]] # left
+                 [7,3,0,4]] # left
         BufferGeometry.__init__(self, vertices=vertices,
                                 indices=[_tri_faces(rect)
                                          for rect in rects],
