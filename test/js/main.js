468b44efb0824ca93966d50731abb3c5b0e9c4a2
Jeffrey Zitelli <zemedi@gmail.com>
2016-03-10 21:45:00 -0500

    webvrapp dep

--- test/js/main.js
+++ test/js/main.js
@@ -1,84 +1,13 @@
-function WebVRApplication(scene, config) {
-    "use strict";
-    this.scene = scene;
-
-    config = config || {};
-    var rendererOptions     = config.rendererOptions;
-    var onResetVRSensor     = config.onResetVRSensor;
-
-    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-    this.renderer = new THREE.WebGLRenderer(rendererOptions);
-    this.renderer.setPixelRatio(window.devicePixelRatio);
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
-
-    var domElement = this.renderer.domElement;
-    document.body.appendChild(domElement);
-    domElement.id = 'webgl-canvas';
-
-    this.vrEffect = new THREE.VREffect(this.renderer, function(error) { console.error('error creating VREffect: ' + error); });
-
-    this.vrControls = new THREE.VRControls(this.camera, function(error) { console.error('error creating VRControls: ' + error); });
-
-    // public methods:
-
-    this.render = function () {
-        this.vrControls.update();
-        this.vrEffect.render(this.scene, this.camera);
-    }.bind(this);
-
-    var wireframeMaterial = new THREE.MeshBasicMaterial({color: 0xeeddaa, wireframe: true});
-    this.toggleWireframe = function () {
-        if (this.scene.overrideMaterial) {
-            this.scene.overrideMaterial = null;
-        } else {
-            this.scene.overrideMaterial = wireframeMaterial;
-        }
-    }.bind(this);
-
-    // button and event handling for WebVR:
-
-    var isPresenting = false;
-
-    window.addEventListener( 'resize', function () {
-        if (!isPresenting) {
-            this.camera.aspect = window.innerWidth / window.innerHeight;
-            this.camera.updateProjectionMatrix();
-            this.renderer.setSize( window.innerWidth, window.innerHeight );
-        }
-    }.bind(this), false );
-
-    var vrButton = document.createElement('button');
-    vrButton.innerHTML = 'ENTER VR';
-    vrButton.style.position = 'absolute';
-    vrButton.style.right = 0;
-    vrButton.style.bottom = 0;
-    vrButton.style.margin = '10px';
-    vrButton.style.padding = '10px';
-    vrButton.style.background = 0x222222;
-    vrButton.style['text-color'] = 0xffffff;
-    vrButton.addEventListener('click', function () {
-        if (!isPresenting) {
-            this.vrEffect.requestPresent().then( function () {
-                isPresenting = true;
-                vrButton.innerHTML = 'EXIT VR';
-            } );
-        } else {
-            this.vrEffect.exitPresent().then( function () {
-                isPresenting = false;
-                vrButton.innerHTML = 'ENTER VR';
-                this.renderer.setSize( window.innerWidth, window.innerHeight )
-            }.bind(this) );
-        }
-    }.bind(this), false);
-    document.body.appendChild(vrButton);
-}
-
-
 function onLoad() {
     "use strict";
 
-    var rS = new rStats({CSSPath: 'rstats/'});
+    var rS;
+    if (URL_PARAMS.rstats) {
+        rS = new rStats({CSSPath: 'rstats/'});
+    } else {
+        function nop() {};
+        rS = function () { return {start: nop, end: nop, frame: nop, tick: nop, update: nop}; };
+    }
 
     var world = new CANNON.World();
     world.gravity.set( 0, -9.8, 0 );
@@ -98,7 +27,10 @@ function onLoad() {
 
         scene.add(avatar);
 
-        app = new WebVRApplication(scene, {rendererOptions: {antialias: !isMobile()}});
+        app = new WebVRApplication(scene, {
+            canvasId: 'webgl-canvas',
+            rendererOptions: {antialias: !isMobile()}
+        });
 
         avatar.add(app.camera);
 
